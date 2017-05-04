93cbcaed4ac2bc8d27faff1cfc87951f4145dc5e
surya-poojary <poojarysurya4@gmail.com>
2017-05-04 11:02:06 +0530

    Hello world git add hello.rb

---
+++ hello.rb
@@ -0,0 +1,14 @@
+#This is a sample Hello World program in Ruby
+#This is a comment,anything followed by a '#' is a comment and IS IGNORED BY THE RUBY INTERPRETER
+#Lets Start
+#puts method automatically prints the output to the screen with a newline
+#You can use print "Hello World \n" but you would be needing to put \n to print a new line
+#No need of parentheses or semicolons in Ruby!!!!
+#Try printing your name by creating the same program as below
+#REMEMEBER RUBY FILES SHOULD END WITH A .rb extension
+#to run it do...ruby [your filename]
+#Thanks for visiting RedasRuby!!
+
+
+
+puts "Hello World, Im Ruby"
